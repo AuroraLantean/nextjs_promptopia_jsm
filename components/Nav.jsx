d0043cfa79132e9facad2f9e231b87f9bc60bc81
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-08-22 13:20:52 +0800

    add NextAuth+Google

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,16 +6,23 @@ import { useEffect, useState } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true; //@@@ 1.10.12
-
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     (async () => {
       const res = await getProviders();
       setProviders(res);
     })();
-  }, []);
+  }, []); //providers are set initially
+
+  console.log(
+    'nav ... session?.user:',
+    session?.user,
+    ', providers:',
+    providers
+  );
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -29,9 +36,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -43,7 +51,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -69,116 +77,42 @@ const Nav = () => {
           </>
         )}
       </div>
-    </nav>
-  );
-};
 
-/*
-const Nav = () => {
-  const { data: session } = useSession();
-
-  const [providers, setProviders] = useState(null);
-  const [toggleDropdown, setToggleDropdown] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getProviders();
-      setProviders(res);
-    })();
-  }, []);
-
-  return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/' className='flex gap-2 flex-center'>
-        <Image
-          src='/assets/images/logo.svg'
-          alt='logo'
-          width={30}
-          height={30}
-          className='object-contain'
-        />
-        <p className='logo_text'>Promptopia</p>
-      </Link>
-
-      {/* Desktop Navigation */
-/*}
-      <div className='sm:flex hidden'>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
         {session?.user ? (
-          <div className='flex gap-3 md:gap-5'>
-            <Link href='/create-prompt' className='black_btn'>
-              Create Post
-            </Link>
-
-            <button type='button' onClick={signOut} className='outline_btn'>
-              Sign Out
-            </button>
-
-            <Link href='/profile'>
-              <Image
-                src={session?.user.image}
-                width={37}
-                height={37}
-                className='rounded-full'
-                alt='profile'
-              />
-            </Link>
-          </div>
-        ) : (
-          <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type='button'
-                  key={provider.name}
-                  onClick={() => {
-                    signIn(provider.id);
-                  }}
-                  className='black_btn'
-                >
-                  Sign in
-                </button>
-              ))}
-          </>
-        )}
-      </div>
-
-      {/* Mobile Navigation */
-/*}
-      <div className='sm:hidden flex relative'>
-        {session?.user ? (
-          <div className='flex'>
+          <div className="flex">
             <Image
               src={session?.user.image}
               width={37}
               height={37}
-              className='rounded-full'
-              alt='profile'
-              onClick={() => setToggleDropdown(!toggleDropdown)}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
-
             {toggleDropdown && (
-              <div className='dropdown'>
+              <div className="dropdown">
                 <Link
-                  href='/profile'
-                  className='dropdown_link'
+                  href="/profile"
+                  className="dropdown_link"
                   onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
-                  href='/create-prompt'
-                  className='dropdown_link'
+                  href="/create-prompt"
+                  className="dropdown_link"
                   onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
                 <button
-                  type='button'
+                  type="button"
                   onClick={() => {
                     setToggleDropdown(false);
                     signOut();
                   }}
-                  className='mt-5 w-full black_btn'
+                  className="mt-5 w-full black_btn"
                 >
                   Sign Out
                 </button>
@@ -190,12 +124,12 @@ const Nav = () => {
             {providers &&
               Object.values(providers).map((provider) => (
                 <button
-                  type='button'
+                  type="button"
                   key={provider.name}
                   onClick={() => {
                     signIn(provider.id);
                   }}
-                  className='black_btn'
+                  className="black_btn"
                 >
                   Sign in
                 </button>
@@ -206,5 +140,5 @@ const Nav = () => {
     </nav>
   );
 };
-*/
+
 export default Nav;
---
+++ utils/database.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export function getCollectionName() {
+  const mongodb_collectionname = process.env.MONGODB_COLLECTION;
+  if (!mongodb_collectionname)
+    return console.log('MONGODB_COLLECTION not found');
+  return mongodb_collectionname;
+}
+
+export const connectToDB = async () => {
+  console.log('connectToDB()...');
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('Mongoose is already connected');
+    return;
+  }
+
+  if (!process.env.MONGODB_USERNAME)
+    return console.log('MONGODB_USERNAME not found');
+  if (!process.env.MONGODB_PASSWORD)
+    return console.log('MONGODB_PASSWORD not found');
+  if (!process.env.MONGODB_CLUSTER)
+    return console.log('MONGODB_CLUSTER not found');
+  if (!process.env.MONGODB_URLX) return console.log('MONGODB_URLX not found');
+
+  try {
+    const dbusername = encodeURIComponent(process.env.MONGODB_USERNAME);
+    const dbpassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+    const connStr = `mongodb+srv://${dbusername}:${dbpassword}@${process.env.MONGODB_CLUSTER}.${process.env.MONGODB_URLX}`;
+    console.log('connStr:', connStr);
+
+    await mongoose.connect(connStr, {
+      dbName: 'share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log('Mongoose connected');
+  } catch (error) {
+    console.log('Mongoose.connect failed:', error);
+  }
+};
